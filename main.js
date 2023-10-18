77479d5a50e3ebe31b34e2ed9164318052bb2688
Kelly Condori <kellycondoricondori@gmail.com>
2023-10-18 15:27:40 -0500

    Adición de la lista de productos a la pantalla

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const menuCarritoIcon=document.querySelector(".navbar-shopping-cart");
 const DesktopMenu =document.querySelector(".desktop-menu");
 const mobileMenu=document.querySelector(".mobile-menu");
 const aside=document.querySelector(".product-detail");
+const cardsContainer=document.querySelector(".cards-container")
  
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
@@ -37,3 +38,72 @@ function toggleCarritoAside(){
 
     aside.classList.toggle("inactive");     
 }
+const productList=[];
+productList.push({
+    name:"Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name:"pantalla",
+    price:620,
+    image:"https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "laptop",
+    price:3650,
+    image:"https://images.pexels.com/photos/3999538/pexels-photo-3999538.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+productList.push({
+    name:"Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+    name:"pantalla",
+    price:620,
+    image:"https://images.pexels.com/photos/777001/pexels-photo-777001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+productList.push({
+    name: "laptop",
+    price:3650,
+    image:"https://images.pexels.com/photos/3999538/pexels-photo-3999538.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+});
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard=document.createElement("div");
+        productCard.classList.add("product-card");
+
+            const productImg=document.createElement("img");
+            productImg.setAttribute("src", product.image);
+
+            const productInfo=document.createElement("div");
+            productInfo.classList.add("product-info");
+                const productIfonDIv=document.createElement("div");
+                    const productPrice=document.createElement("p");
+                    productPrice.innerText="$" + product.price;
+                    const productName=document.createElement("p");
+                    productName.innerText=product.name;
+                productIfonDIv.appendChild(productPrice);
+                productIfonDIv.appendChild(productName);
+                
+                const productInfoFigure=document.createElement("figure");
+                    const iconCarrito=document.createElement("img"); /* la constante en la clase del profesor se llama: productImgCart*/
+                    iconCarrito.setAttribute("src", "./icons/bt_add_to_cart.svg");
+                productInfoFigure.appendChild(iconCarrito);
+            productInfo.appendChild(productIfonDIv);
+            productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);        
+
+    }
+}
+renderProducts(productList);
+
