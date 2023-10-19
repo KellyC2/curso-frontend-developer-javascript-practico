f5dd36315229aac82a56010ddc8d0b6eb5dddcd1
Kelly Condori <kellycondoricondori@gmail.com>
2023-10-19 15:59:11 -0500

    Integración de la pantalla de detalles del producto ubicado en mi lista de producto

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuCarritoIcon=document.querySelector(".navbar-shopping-cart");
 
 const DesktopMenu =document.querySelector(".desktop-menu");
 const mobileMenu=document.querySelector(".mobile-menu");
-const aside=document.querySelector(".product-detail");
+const shoppingCartContainer=document.querySelector("#shoppingCartContainer");
 const cardsContainer=document.querySelector(".cards-container")
  
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -13,16 +13,16 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    const isAsideClosed= aside.classList.contains("inactive");
+    const isAsideClosed= shoppingCartContainer.classList.contains("inactive");
     if(!isAsideClosed){
-        aside.classList.add("inactive");
+        shoppingCartContainer.classList.add("inactive");
     }    
     DesktopMenu.classList.toggle("inactive");
 }
 function toggleMobileMenu(){
-    const isAsideClosed= aside.classList.contains("inactive");
+    const isAsideClosed= shoppingCartContainer.classList.contains("inactive");
     if(!isAsideClosed){
-        aside.classList.add("inactive");
+        shoppingCartContainer.classList.add("inactive");
     }
     mobileMenu.classList.toggle("inactive");
 }
@@ -36,7 +36,7 @@ function toggleCarritoAside(){
         DesktopMenu.classList.add("inactive");
     }
 
-    aside.classList.toggle("inactive");     
+    shoppingCartContainer.classList.toggle("inactive");     
 }
 const productList=[];
 productList.push({
