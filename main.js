07c7ecbb3fbcc972ff66027067190abd6f157033
Kelly Condori <kellycondoricondori@gmail.com>
2023-10-19 18:12:21 -0500

    Dinamismo de la pantalla, detalles de producto, con Javascript e integración de todos los componentes de mi proyecto

--- main.js
+++ main.js
@@ -1,43 +1,61 @@
 const menuEmail=document.querySelector(".navbar-email");
 const menuHamIcon=document.querySelector(".menu");
 const menuCarritoIcon=document.querySelector(".navbar-shopping-cart");
-
+const productDetailClosedIcon=document.querySelector(".product-detail-close");
 
 const DesktopMenu =document.querySelector(".desktop-menu");
 const mobileMenu=document.querySelector(".mobile-menu");
 const shoppingCartContainer=document.querySelector("#shoppingCartContainer");
-const cardsContainer=document.querySelector(".cards-container")
+const cardsContainer=document.querySelector(".cards-container");
+const productDetailContainer=document.querySelector("#productDetail");
  
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+productDetailClosedIcon.addEventListener("click", closeProductDetailAside);
 
 function toggleDesktopMenu(){
     const isAsideClosed= shoppingCartContainer.classList.contains("inactive");
-    if(!isAsideClosed){
+    const isProductDetailContainerclosed=productDetailContainer.classList.contains("inactive");
+    if(!isAsideClosed||!isProductDetailContainerclosed){
+        productDetailContainer.classList.add("inactive");
         shoppingCartContainer.classList.add("inactive");
     }    
     DesktopMenu.classList.toggle("inactive");
 }
 function toggleMobileMenu(){
     const isAsideClosed= shoppingCartContainer.classList.contains("inactive");
-    if(!isAsideClosed){
+    const isProductDetailContainerclosed=productDetailContainer.classList.contains("inactive");
+
+    if(!isAsideClosed||!isProductDetailContainerclosed){
         shoppingCartContainer.classList.add("inactive");
+        productDetailContainer.classList.add("inactive");
     }
     mobileMenu.classList.toggle("inactive");
 }
-
 function toggleCarritoAside(){
     const isMobileMenuClosed=mobileMenu.classList.contains("inactive");
-    const isDesktopMenuClosed= menuEmail.classList.contains("inactive")
+    const isDesktopMenuClosed= menuEmail.classList.contains("inactive");
+    const isProductDetailContainerclosed=productDetailContainer.classList.contains("inactive");
 
-    if(!isMobileMenuClosed||!isDesktopMenuClosed ){
+    if(!isMobileMenuClosed||!isDesktopMenuClosed||!isProductDetailContainerclosed){
         mobileMenu.classList.add("inactive");
         DesktopMenu.classList.add("inactive");
+        productDetailContainer.classList.add("inactive");
     }
 
     shoppingCartContainer.classList.toggle("inactive");     
 }
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add("inactive");
+    DesktopMenu.classList.add("inactive");
+    mobileMenu.classList.add("inactive");
+    productDetailContainer.classList.remove("inactive");
+}
+function closeProductDetailAside(){
+    productDetailContainer.classList.add("inactive");
+}
+
 const productList=[];
 productList.push({
     name:"Bike",
@@ -81,6 +99,7 @@ function renderProducts(arr){
 
             const productImg=document.createElement("img");
             productImg.setAttribute("src", product.image);
+            productImg.addEventListener("click", openProductDetailAside);
 
             const productInfo=document.createElement("div");
             productInfo.classList.add("product-info");
@@ -106,4 +125,6 @@ function renderProducts(arr){
     }
 }
 renderProducts(productList);
+
+
 
